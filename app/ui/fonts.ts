34f1aecf6dffa802f92e05afbe0ed8698b1de50b
yeah1tnt <pichip22@hotmail.com>
2023-11-25 16:32:05 -0500

    lesson 6 start

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import {Inter, Lusitana} from 'next/font/google';
+
+export const inter = Inter({
+    subsets: ['latin'],
+    variable: '--font-inter',
+})
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+    variable: '--font-lusitana',
+})
